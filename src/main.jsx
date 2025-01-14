4b8826beae88044e556630a201f6d44bb734c0b0
Luan <yluanpvp@gmail.com>
2025-01-14 11:52:02 -0300

    feat: created the page of admin and side menu with options to navigate with screens

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ToastContainer } from "react-toastify";
-import { RouterProvider } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
-import { router } from "./routes";
+import { Router } from "./routes";
 import GlobalStyles from "./styles/globalStyles";
 import AppProvider from "./hooks";
 import { Elements } from "@stripe/react-stripe-js";
@@ -13,7 +13,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AppProvider>
       <Elements stripe={stripePromise}>
-        <RouterProvider router={router} />
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
       </Elements>
       <GlobalStyles />
       <ToastContainer
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,74 +1,40 @@
-import { createBrowserRouter } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 import { Login } from "../containers/Login";
 import { Register } from "../containers/Register";
 import { Home } from "../containers/Home";
 import { Menu } from "../containers/Menu";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
 import Cart from "../containers/Cart";
 import Checkout from "../containers/Checkout";
 import CompletePayment from "../containers/CompletePayment";
+import { UserLayout } from "../layouts/UserLayout";
+import { AdminLayout } from "../layouts/AdminLayout";
+import { Admin } from "../containers/Admin";
+import { Products } from "../containers/Admin/Products";
+import { Orders } from "../containers/Admin/Orders";
+import { NewProduct } from "../containers/Admin/NewProduct";
+import { EditProduct } from "../containers/Admin/EditProduct";
 
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <>
-        <Header />
-        <Home />
-        <Footer />
-      </>
-    ),
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/cadastro",
-    element: <Register />,
-  },
-  {
-    path: "/cardapio",
-    element: (
-      <>
-        <Header />
-        <Menu />
-        <Footer />
-      </>
-    ),
-  },
-  {
-    path: "/carrinho",
-    element: (
-      <>
-        <Header />
-        <Cart />
-        <Footer />
-      </>
-    ),
-  },
+export function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<UserLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/cardapio" element={<Menu />} />
+        <Route path="/carrinho" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/complete" element={<CompletePayment />} />
+      </Route>
 
-  {
-    path: "/checkout",
-    element: (
-      <>
-        <Header />
-        <Checkout />
-        <Footer />
-      </>
-    ),
-  },
+      <Route path="/admin" element={<AdminLayout />}>
+        <Route path="/admin/pedidos" element={<Orders />} />
+        <Route path="/admin/novo-produto" element={<NewProduct />} />
+        <Route path="/admin/editar-produto" element={<EditProduct />} />
+        <Route path="/admin/produtos" element={<Products />} />
+      </Route>
 
-  {
-    path: "/complete",
-    element: (
-      <>
-        <Header />
-        <CompletePayment />
-        <Footer />
-      </>
-    ),
-  },
-]);
+      <Route path="/login" element={<Login />} />
+      <Route path="/cadastro" element={<Register />} />
+    </Routes>
+  );
+}
