2fa056a65db9bbb79c39173e61d4b76f402a0eb8
Luan <yluanpvp@gmail.com>
2025-01-14 11:52:40 -0300

    feat: create the page admin of orders with filters and can change the state of prepare

---
+++ src/utils/formatDate.js
@@ -0,0 +1,8 @@
+export function formatDate(date) {
+    return new Date(date).toLocaleDateString('pt-BR', {
+        hour: '2-digit',
+        minute: '2-digit',
+        month: 'short',
+        day: '2-digit',
+    })
+}
